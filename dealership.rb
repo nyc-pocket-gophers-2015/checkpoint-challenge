9f064fbe1db32f6a61acc97a64f9f4a27c7e883e
Siddharth Watal <siddharthwatal@Siddharths-MacBook-Air.local>
2015-04-15 12:13:37 -0400

    add add and remove functionality. refactor all printing up into car class

--- dealership.rb
+++ dealership.rb
@@ -8,46 +8,46 @@ class Car
     @inventory_number = args[:inventory_number]
     @year = args[:year]
   end
+
+  def to_s
+    "#{year} #{make} #{model} ID: #{inventory_number}"
+  end
 end
 
 class Dealership
-
   def initialize(cars)
     @cars = cars
   end
 
-  def cars
-    @cars.each do |car|
-      pretty_print_car(car)
-    end
-  end
-
-  def pretty_print_car(car)
-    puts "#{car.year} #{car.make} #{car.model} ID: #{car.inventory_number}"
-  end
-
-  def print_group(array)
-    array.each { |car| pretty_print_car(car) }
-  end
 
   def cars
-    print_group(@cars)
+    @cars.each {|car| car}
+  end
+
+  def remove(id)
+    @cars.delete_if{|car| car.inventory_number == id}
+    CarLoader.write_to_csv("output.csv", @cars)
+  end
+
+  def add(hash)
+    @cars << Car.new(hash)
+    CarLoader.write_to_csv("output.csv", @cars)
   end
 
   def find_make(make)
-    print_group(@cars.select { |car| car.make == make })
+    @cars.select{ |car| car.make == make }
   end
 
   def older_than(year)
-    print_group(@cars.select { |car| car.year < year })
+    @cars.select { |car| car.year < year }
   end
 
   def newer_than(year)
-    print_group(@cars.select { |car| car.year > year })
+    @cars.select { |car| car.year > year }
   end
 
   def newest_car
-    pretty_print_car(@cars.sort_by{|car| car.year}.last)
+    @cars.sort_by{|car| car.year}.last
   end
 end
 
@@ -58,9 +58,15 @@ module CarLoader
     cars.map! {|element| Car.new(element)}
   end
 
-  def self.write_to_csv
-
+  def self.write_to_csv(filepath, array)
+    CSV.open(filepath, "w") do |file|
+      file.puts ["inventory_number","make","model","year"]
+      array.each do |car|
+        file.puts ([car.inventory_number, car.make, car.model, car.year])
+      end
+    end
   end
+
 end
 
 cars = CarLoader.get_cars_from_csv("inventory.csv")
@@ -68,23 +74,27 @@ dealership = Dealership.new(cars)
 
 if ARGV[0] == "find"
   if ARGV[1] == "all"
-    # print all of the cars on Deano's lot
-    dealership.cars
+    puts dealership.cars
   elsif ARGV[1] == "make"
-    # print cars of the make supplied in ARGV[2]
-    dealership.find_make(ARGV[2])
+    puts dealership.find_make(ARGV[2].capitalize)
   elsif ARGV[1] == "pre"
-    # print cars made before the year supplied in ARGV[2]
-    dealership.older_than(ARGV[2])
+    puts dealership.older_than(ARGV[2])
   elsif ARGV[1] == "post"
-    # print cars made after the year supplied in ARGV[2]
-    dealership.newer_than(ARGV[2])
+    puts dealership.newer_than(ARGV[2])
   elsif ARGV[1] == "newest"
-    # print the newest car on the lot
-    dealership.newest_car
+    puts dealership.newest_car
   end
 elsif ARGV[0] == "remove"
-
+  removal_id = ARGV[1]
+  dealership.remove(removal_id)
+elsif ARGV[0] == "add"
+  car_hash = {
+    inventory_number: ARGV[1],
+    make: ARGV[2],
+    model: ARGV[3],
+    year: ARGV[4]
+  }
+  dealership.add(car_hash)
 end
 
 
