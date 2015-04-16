ecf62a7c821f7db4be056b4bbe38075dad85e22a
Nikolai Calder <redlacialokin@gmail.com>
2015-04-15 21:40:31 -0400

    refactor based on feedback from code recview

--- dealership.rb
+++ dealership.rb
@@ -1,34 +1,61 @@
+require 'csv'
+
 class Car
-  # I need to encapsulate these objects inside the dealership...
+  attr_reader :inventory_number, :make, :year, :model
+  def initialize(attributes = {})
+    @inventory_number = attributes[:inventory_number]
+    @make =  attributes[:make]
+    @model =  attributes[:model]
+    @year =  attributes[:year]
+  end
+
+  def to_s
+    string = ""
+    string += "#{self.year}  #{self.make} #{self.model} ID: #{self.inventory_number}"
+  end
 end
 
 class Dealership
+  attr_reader :cars
   def initialize(cars = nil)
     @cars = cars || []
   end
 
   def find_make(make)
     cars_by_make = []
-    @cars.each do |car|
+    cars.each do |car|
       cars_by_make << car if car.make == make
     end
     cars_by_make
   end
 
+  def pre(year)
+    cars.select {|car| car.year < year}
+  end
+
+  def post(year)
+     cars.select {|car| car.year > year}
+  end
+
+  def all
+   cars
+  end
+
   def newest_car
-    # I need to return the car on the lot that is the newest...
+    cars.max_by {|car|  car.year}
   end
 end
 
 module CarLoader
   def self.get_cars_from_csv(filepath)
-    # The result is being passed to the new dealership.
-    # I need to return some useful data from this method...
+    CSV.read(filepath, :headers => true,:header_converters => :symbol).map { |row| Car.new(row) }
   end
 end
 
 cars = CarLoader.get_cars_from_csv("inventory.csv")
 dealership = Dealership.new(cars)
+ dealership.find_make("Honda")
+
 
 if ARGV[0] == "find"
   if ARGV[1] == "all"
@@ -39,9 +66,13 @@ if ARGV[0] == "find"
     puts dealership.find_make(ARGV[2])
   elsif ARGV[1] == "pre"
     # print cars made before the year supplied in ARGV[2]
+   puts dealership.pre(ARGV[2])
   elsif ARGV[1] == "post"
     # print cars made after the year supplied in ARGV[2]
+    puts dealership.post(ARGV[2])
   elsif ARGV[1] == "newest"
     # print the newest car on the lot
+    puts dealership.newest_car
+
   end
 end
--- dealership_spec.rb
+++ dealership_spec.rb
@@ -5,16 +5,16 @@ end
 
 describe CarLoader do
   describe "#get_cars_from_csv" do
-    xit "returns an array of Car objects" do
-      # change 'xit' to 'it' and finish this test
+    it "returns an array of Car objects" do
+      expect(CarLoader.get_cars_from_csv("inventory.csv")).to be_a(Array)
     end
   end
 end
 
 describe Dealership do
-  let(:cars) {[ Car.new(make: "Honda"),
-                Car.new(make: "Toyota"),
-                Car.new(make: "Honda")]}
+  let(:cars) {[ Car.new(make: "Honda" ,year: 2014),
+                Car.new(make: "Toyota", year: 2002),
+                Car.new(make: "Honda", year: 1999)]}
 
   let(:dealership) { Dealership.new(cars)}
 
@@ -23,9 +23,38 @@ describe Dealership do
       expect(dealership.find_make("Honda").size).to eq(2)
     end
 
-    xit "returns an empty array when there are no cars of the given make" do
-      # change 'xit' to 'it' and finish this test
+    it "finds all cars of a given make" do
+      expect(dealership.find_make("Toyota").size).to eq(1)
     end
+
+      it "returns an empty array when there are no cars of the given make" do
+       expect(dealership.find_make("DBC").size).to eq(0)
+     end
+
+     describe "#all" do
+        it "should return all cars to user" do
+          expect(dealership.all.size).to eq(3)
+       end
+
+       describe "#newest" do
+          it "should return newest car to user" do
+            expect(dealership.newest_car.year).to eq(2014)
+          end
+       end
+
+
+       describe "pre" do
+          it "should return all car objects before a certain year " do
+            expect(dealership.pre(2014).size).to eq(2)
+          end
+       end
+
+       describe "post" do
+          it "should return all car objects after a certain year " do
+            expect(dealership.post(1999).size).to eq(2)
+          end
+       end
+     end
   end
 end
 
